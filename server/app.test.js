c1216873e590bbecc1350d1b2f09122dfb95b591
Prachiti Atigre <prachiti.atigre@gmail.com>
2022-11-28 16:20:33 -0500

    4:20pm - added descriptions to test cases

--- server/app.test.js
+++ server/app.test.js
@@ -4,7 +4,6 @@ import app from './index.js'
 
 describe("POST/login", () => {
     describe("given a username and password", () => {
-
         test("login company successfully", async() => {
             await request(app).post("/login").send({
                 username: "pie123",
@@ -47,7 +46,7 @@ describe("POST/login", () => {
 })
 
 describe("POST /register", () => {
-    describe("given a fullName, username, password, passwordAgain, role", () => {
+    describe("given a fullName, username, password, password again, role", () => {
 
         test("username already exists", async() => {
             await request(app).post("/register").send({
@@ -102,47 +101,51 @@ describe("POST /register", () => {
 })
 
 describe("POST/insertInsurancePlan", () => {
-    test("insurance company already exists", async() => {
-        await request(app).post("/insertInsurancePlan").send({
-            insuranceName: "AAA",
-            insuranceType: "Auto",
-            planName: "Silver",
-            yearlyCost: "3000",
-            maxAge: 100,
-            coverageDetails: [{coverageName: "Tires Replacement", coverageAmount: "800"}, 
-                                {coverageName: "Servicing", coverageAmount: "1500"}]
-        }).then(function (response) {
-            expect(response.text).toBe("{\"redirect\":\"insurance_already_exists\",\"insuranceType\":\"Auto\"}")
+    describe("given an insurance name, insurance type, plan name, yearly cost, max age and coverage details that include the coverage name and amount", () => {
+        test("insurance company already exists", async() => {
+            await request(app).post("/insertInsurancePlan").send({
+                insuranceName: "AAA",
+                insuranceType: "Auto",
+                planName: "Silver",
+                yearlyCost: "3000",
+                maxAge: 100,
+                coverageDetails: [{coverageName: "Tires Replacement", coverageAmount: "800"}, 
+                                    {coverageName: "Servicing", coverageAmount: "1500"}]
+            }).then(function (response) {
+                expect(response.text).toBe("{\"redirect\":\"insurance_already_exists\",\"insuranceType\":\"Auto\"}")
+            })
         })
-    })
 
-    // -------------- WORKS --------------
-    // test("insurance company already exists", async() => {
-    //     await request(app).post("/updateInsurancePlan").send({
-    //         insuranceName: "Health For All",
-    //         insuranceType: "Health",
-    //         planName: "Silver",
-    //         yearlyCost: "2000",
-    //         maxAge: 100,
-    //         coverageDetails: [{coverageName: "Dental", coverageAmount: "100"}, 
-    //                             {coverageName: "Vision", coverageAmount: "85"}]
-    //     }).then(function (response) {
-    //         expect(response.text).toBe("{\"redirect\":\"new_insurance_added_successfully\"}")
-    //     })
-    // })    
+        // -------------- WORKS --------------
+        // test("insurance company already exists", async() => {
+        //     await request(app).post("/updateInsurancePlan").send({
+        //         insuranceName: "Health For All",
+        //         insuranceType: "Health",
+        //         planName: "Silver",
+        //         yearlyCost: "2000",
+        //         maxAge: 100,
+        //         coverageDetails: [{coverageName: "Dental", coverageAmount: "100"}, 
+        //                             {coverageName: "Vision", coverageAmount: "85"}]
+        //     }).then(function (response) {
+        //         expect(response.text).toBe("{\"redirect\":\"new_insurance_added_successfully\"}")
+        //     })
+        // })    
+    })
 })
 
 describe("POST/updateInsurancePlan", () => {
-    test("company benefits are updated", async() => {
-        await request(app).post("/updateInsurancePlan").send({
-            insuranceName: "pie123", //It takes username as input, finds full name in user db and sets that as insurance name
-            insuranceType: "Health",
-            planName: "Gold",
-            yearlyCost: "900",
-            maxAge: "50",
-            coverageDetails: [{coverageName:"Dental", coverageAmount: "200"}]
-        }).then(function (response) {
-            expect(response.text).toBe("{\"redirect\":\"updated_company_insurance\"}")
+    describe("given the company username, insurance type, plan name, yearly cost, max age and coverage details that include the coverage name and amount", () => {
+        test("company benefits are updated", async() => {
+            await request(app).post("/updateInsurancePlan").send({
+                insuranceName: "pie123", //It takes username as input, finds full name in user db and sets that as insurance name
+                insuranceType: "Health",
+                planName: "Gold",
+                yearlyCost: "900",
+                maxAge: "50",
+                coverageDetails: [{coverageName:"Dental", coverageAmount: "200"}]
+            }).then(function (response) {
+                expect(response.text).toBe("{\"redirect\":\"updated_company_insurance\"}")
+            })
         })
     })
 })
@@ -162,37 +165,43 @@ describe("POST/updateInsurancePlan", () => {
 // })
 
 describe("POST/getOfferedInsurances", () => {
-    test("company views plans offered by them", async() => {
-        await request(app).post("/getOfferedInsurances").send({
-            username: "AAA"
-        }).then(function (response) {
-            expect(response.text).toBe("[{\"plans\":[{\"planName\":\"Premium\",\"yearlyCost\":\"5000\",\"age\":\"100\",\"coverages\":[{\"coverageName\":\"Tires Replacement\",\"coverageAmount\":\"1000\"},{\"coverageName\":\"Servicing\",\"coverageAmount\":\"2000\"}]}],\"_id\":\"637fc80468dc4f7ded025891\",\"insuranceName\":\"AAA\",\"insuranceType\":\"Auto\",\"__v\":0}]")
+    describe("given the company username", () => {
+        test("company views plans offered by them", async() => {
+            await request(app).post("/getOfferedInsurances").send({
+                username: "AAA"
+            }).then(function (response) {
+                expect(response.text).toBe("[{\"plans\":[{\"planName\":\"Premium\",\"yearlyCost\":\"5000\",\"age\":\"100\",\"coverages\":[{\"coverageName\":\"Tires Replacement\",\"coverageAmount\":\"1000\"},{\"coverageName\":\"Servicing\",\"coverageAmount\":\"2000\"}]}],\"_id\":\"637fc80468dc4f7ded025891\",\"insuranceName\":\"AAA\",\"insuranceType\":\"Auto\",\"__v\":0}]")
+            })
         })
     })
 })
 
 // -------------- WORKS --------------
 // describe("POST/addAdditionalPlansToInsurance", () => {
-//     test("add more plans", async() => {
-//         await request(app).post("/addAdditionalPlansToInsurance").send({
-//             username: "AAA",
-//             planName: "Silver",
-//             yearlyCost: "2500",
-//             age: "100",
-//             coverages: [{coverageName:"Dental", coverageAmount: "200"}]
-//         }).then(function (response) {
-//             expect(response.text).toBe("{\"redirect\":\"added_additional_plan_to_insurance\"}")
+//     describe("given the company username, plan name, yearly cost, age and coverage details that include the coverage name and amount", () => {
+//         test("add more plans", async() => {
+//             await request(app).post("/addAdditionalPlansToInsurance").send({
+//                 username: "AAA",
+//                 planName: "Silver",
+//                 yearlyCost: "2500",
+//                 age: "100",
+//                 coverages: [{coverageName:"Dental", coverageAmount: "200"}]
+//             }).then(function (response) {
+//                 expect(response.text).toBe("{\"redirect\":\"added_additional_plan_to_insurance\"}")
+//             })
 //         })
 //     })
 // })
 
 describe("POST/deleteOfferedInsurance", () => {
-    test("delete offered insurance", async() => {
-        await request(app).post("/deleteOfferedInsurance").send({
-            username: "americancross",
-            planName: "Silver",
-        }).then(function (response) {
-            expect(response.text).toBe("{\"redirect\":\"deleted_offered_insurance\"}")
+    describe("given the company username and plan name", () => {
+        test("delete offered insurance", async() => {
+            await request(app).post("/deleteOfferedInsurance").send({
+                username: "americancross",
+                planName: "Silver",
+            }).then(function (response) {
+                expect(response.text).toBe("{\"redirect\":\"deleted_offered_insurance\"}")
+            })
         })
     })
 })
@@ -200,28 +209,32 @@ describe("POST/deleteOfferedInsurance", () => {
 // ------------------------------------------------------------- CUSTOMER ------------------------------------------------------------- 
 
 describe("POST/deleteMyInsurance", () => {
-    test("delete my insurance", async() => {
-        await request(app).post("/deleteMyInsurance").send({
-            username: "prachi",
-            insuranceName: "Pie"
-        }).then(function (response) {
-            expect(response.text).toBe("{\"redirect\":\"successfully_deleted_insurance_by_customer\"}")
+    describe("given the customer's username and insurance name that they are enrolled in", () => {
+        test("delete my insurance", async() => {
+            await request(app).post("/deleteMyInsurance").send({
+                username: "prachi",
+                insuranceName: "Pie"
+            }).then(function (response) {
+                expect(response.text).toBe("{\"redirect\":\"successfully_deleted_insurance_by_customer\"}")
+            })
         })
     })
 })
 
 // -------------- WORKS --------------
 // describe("POST/addInsuranceToUser", () => {
-//     test("add insurance for customer", async() => {
-//         await request(app).post("/addInsuranceToUser").send({
-//             username: "prachitiatigre",
-//             insuranceName: "American Cross",
-//             insuranceType: "Health",
-//             planName: "Gold",
-//             yearlyCost: "2000",
-//             coverages: [{coverageName:"Physical", coverageAmount: "100"}],
-//         }).then(function (response) {
-//             expect(response.text).toBe("{\"redirect\":\"added_insurance_to_user\"}")
+//     describe("given the customer's username, insurance name, insurance type, plan name, yearly cost and coverage details", () => {
+//         test("add insurance for customer", async() => {
+//             await request(app).post("/addInsuranceToUser").send({
+//                 username: "prachitiatigre",
+//                 insuranceName: "American Cross",
+//                 insuranceType: "Health",
+//                 planName: "Gold",
+//                 yearlyCost: "2000",
+//                 coverages: [{coverageName:"Physical", coverageAmount: "100"}],
+//             }).then(function (response) {
+//                 expect(response.text).toBe("{\"redirect\":\"added_insurance_to_user\"}")
+//             })
 //         })
 //     })
 // })
@@ -236,21 +249,25 @@ describe("GET/getAvailableInsuranceTypes", () => {
 })
 
 describe("POST/testFilter", () => {
-    test("test filter", async() => {
-        await request(app).post("/testFilter").send({
-            insuranceType: "Auto",
-        }).then(function (response) {
-            expect(response.text).toBe("[{\"plans\":[{\"planName\":\"Premium\",\"yearlyCost\":\"5000\",\"age\":\"100\",\"coverages\":[{\"coverageName\":\"Tires Replacement\",\"coverageAmount\":\"1000\"},{\"coverageName\":\"Servicing\",\"coverageAmount\":\"2000\"}]}],\"_id\":\"637fc80468dc4f7ded025891\",\"insuranceName\":\"AAA\",\"insuranceType\":\"Auto\",\"__v\":0}]")
+    describe("given the insurance type", () => {
+        test("test filter", async() => {
+            await request(app).post("/testFilter").send({
+                insuranceType: "Auto",
+            }).then(function (response) {
+                expect(response.text).toBe("[{\"plans\":[{\"planName\":\"Premium\",\"yearlyCost\":\"5000\",\"age\":\"100\",\"coverages\":[{\"coverageName\":\"Tires Replacement\",\"coverageAmount\":\"1000\"},{\"coverageName\":\"Servicing\",\"coverageAmount\":\"2000\"}]}],\"_id\":\"637fc80468dc4f7ded025891\",\"insuranceName\":\"AAA\",\"insuranceType\":\"Auto\",\"__v\":0}]")
+            })
         })
     })
 })
 
-describe("POST /getUserInsurances", () => {
-    test("customer benefits are updated", async() => {
-        await request(app).post("/getUserInsurances").send({
-            username: "prachitiatigre"
-        }).then(function (response) {
-            expect(response.text).toBe("{\"enrolledIn\":[{\"insuranceName\":\"American Cross\",\"insuranceType\":\"Health\",\"planName\":\"Gold\",\"yearlyCost\":\"2000\",\"coverages\":[{\"coverageName\":\"Physical\",\"coverageAmount\":\"100\"}]},{\"insuranceName\":\"American Cross\",\"insuranceType\":\"Health\",\"planName\":\"Gold\",\"yearlyCost\":\"2000\",\"coverages\":[{\"coverageName\":\"Physical\",\"coverageAmount\":\"100\"}]}],\"_id\":\"63699c0cef98ffed67dacf43\",\"fullName\":\"Prachiti Atigre\",\"username\":\"prachitiatigre\",\"password\":\"123\",\"role\":\"customer\",\"__v\":0}")
+describe("POST/getUserInsurances", () => {
+    describe("given the customer's username", () => {
+        test("customer benefits are updated", async() => {
+            await request(app).post("/getUserInsurances").send({
+                username: "prachitiatigre"
+            }).then(function (response) {
+                expect(response.text).toBe("{\"enrolledIn\":[{\"insuranceName\":\"American Cross\",\"insuranceType\":\"Health\",\"planName\":\"Gold\",\"yearlyCost\":\"2000\",\"coverages\":[{\"coverageName\":\"Physical\",\"coverageAmount\":\"100\"}]},{\"insuranceName\":\"American Cross\",\"insuranceType\":\"Health\",\"planName\":\"Gold\",\"yearlyCost\":\"2000\",\"coverages\":[{\"coverageName\":\"Physical\",\"coverageAmount\":\"100\"}]}],\"_id\":\"63699c0cef98ffed67dacf43\",\"fullName\":\"Prachiti Atigre\",\"username\":\"prachitiatigre\",\"password\":\"123\",\"role\":\"customer\",\"__v\":0}")
+            })
         })
     })
 })
